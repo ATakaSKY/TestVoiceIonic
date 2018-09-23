18d5bac894edd3164db9d5eadf4c96f465ad22c6
Thakur <aakthakur@deloitte.com>
2018-09-23 16:07:19 +0530

    test v1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(
+    public navCtrl: NavController,
+    private speechRecognition: SpeechRecognition
+  ) {}
 
-  constructor(public navCtrl: NavController) {
-
+  feature() {
+    // Check feature available
+    this.speechRecognition
+      .isRecognitionAvailable()
+      .then((available: boolean) => alert(available));
   }
 
+  startListening() {
+    // Start the recognition process
+    this.speechRecognition
+      .startListening({})
+      .subscribe(
+        (matches: Array<string>) => alert(JSON.stringify(matches)),
+        onerror => alert(JSON.stringify(onerror))
+      );
+  }
 }
